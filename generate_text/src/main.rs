4f9aba48d3104fa2dd31b84acaf6c06a91ccca20
Alex Bzdel <ab913@duke.edu>
2023-05-04 17:23:58 +0000

    format

--- generate_text/src/main.rs
+++ generate_text/src/main.rs
@@ -4,15 +4,16 @@ use rust_bert::pipelines::generation_utils::LanguageGenerator;
 use std::io;
 
 fn main() -> anyhow::Result<()> {
-
     let model = GPT2Generator::new(Default::default())?;
 
     println!("Enter some text for the model to generate from:");
     let mut input_context = String::new();
- 
-    io::stdin().read_line(&mut input_context).expect("failed to readline");
 
-    let output = model.generate(Some(vec!(input_context).as_ref()), None);
+    io::stdin()
+        .read_line(&mut input_context)
+        .expect("failed to readline");
+
+    let output = model.generate(Some(vec![input_context].as_ref()), None);
 
     println!("{:?}", output);
 
